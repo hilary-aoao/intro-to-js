c5f051d2894b164e0c6ee4be0841c4e3bba0017e
hilary-aoao <69435811+hilary-aoao@users.noreply.github.com>
2022-06-23 20:29:40 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("My name is Hilary")
+alert("I am 179cm tall")
+alert("I am a Nigerian")
+
+
+document.write("My name is Hilary. ")
+document.write("I am 179cm tall. ")
+document.write("I am a Nigerian. ")
